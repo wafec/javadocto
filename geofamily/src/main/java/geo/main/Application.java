253daae9f3679def39016f3fc5a80237aab491e2
Wallace Cardoso <ra144685@students.ic.unicamp.br>
2017-10-03 21:23:24 -0300

    GEO : first commit

---
+++ geofamily/src/main/java/geo/main/Application.java
@@ -0,0 +1,7 @@
+package geo.main;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ geofamily/src/test/java/geo/main/ApplicationTest.java
@@ -0,0 +1,4 @@
+package geo.main;
+
+public class ApplicationTest {
+}
