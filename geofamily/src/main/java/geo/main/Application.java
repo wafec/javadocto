a93e63f0bd1a22238351e8a1916bf89b9fe8cd33
Wallace Cardoso <ra144685@students.ic.unicamp.br>
2017-10-05 02:59:30 +0000

    GEO : improvements

--- geofamily/src/main/java/geo/main/Application.java
+++ geofamily/src/main/java/geo/main/Application.java
@@ -8,15 +8,15 @@ import geo.problem.Booth;
 public class Application {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        Geo geo = new Geo(
+        Geo geo = new GeoVar(
                 1.0,
                 50000,
                 new Booth.ObjectiveOne(),
                 Booth.searchDomain);
         geo.run();
         System.out.println("The End!");
-        int x = (geo.getBestSequence().getProjectVariables()[0].getValue() - 10);
-        int y = (geo.getBestSequence().getProjectVariables()[1].getValue() - 10);
+        double x = Booth.getX(geo.getBestSequence());
+        double y = Booth.getY(geo.getBestSequence());
         System.out.println("x=" + x + ", y=" + y);
         System.out.println("Best iteration is " + geo.getBestIteration());
     }
--- geofamily/src/main/java/geo/problem/Booth.java
+++ geofamily/src/main/java/geo/problem/Booth.java
@@ -5,20 +5,32 @@ import geo.algorithm.Objective;
 import geo.algorithm.Sequence;
 
 public class Booth {
-    public static final int numberOfBits = (int)(Math.ceil(Math.log(20) / Math.log(2)));
+    public static final int decimalPlaces = 1;
+    private static final int upperBound = (int)(2 * Math.pow(10, decimalPlaces + 1));
+    public static final int numberOfBits = (int)(Math.ceil(Math.log(upperBound) / Math.log(2)));
     public static final BinaryInteger.Domain[] searchDomain = new BinaryInteger.Domain[] {
-            new BinaryInteger.Domain(0, 20, numberOfBits),
-            new BinaryInteger.Domain(0, 20, numberOfBits)
+            new BinaryInteger.Domain(0, upperBound, numberOfBits),
+            new BinaryInteger.Domain(0, upperBound, numberOfBits)
     };
+
+    public static double getX(Sequence sequence) {
+        int x = sequence.getProjectVariables()[0].getValue() - upperBound / 2;
+        double dx = x / Math.pow(10, decimalPlaces);
+        return dx;
+    }
+
+    public static double getY(Sequence sequence) {
+        int y = sequence.getProjectVariables()[1].getValue() - upperBound / 2;
+        double dy = y / Math.pow(10, decimalPlaces);
+        return dy;
+    }
 
     public static class ObjectiveOne implements Objective {
         @Override
         public double eval(Object object) {
             Sequence sequence = (Sequence) object;
-            int x = sequence.getProjectVariables()[0].getValue() - 10;
-            int y = sequence.getProjectVariables()[1].getValue() - 10;
-            double dx = x / 1.0;
-            double dy = y / 1.0;
+            double dx = getX(sequence);
+            double dy = getY(sequence);
             double fx = Math.pow((dx + 2 * dy - 7), 2) + Math.pow((2 * dx + dy - 5), 2);
             return fx;
         }
