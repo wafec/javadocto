e084e82ce754384484e6587f495d5a8541adb72f
Wallace <wallacefcardoso@gmail.com>
2019-05-05 19:34:00 -0300

    ANA: del instead of rm

--- osdsn2-analytics/resources/flow.bat
+++ osdsn2-analytics/resources/flow.bat
@@ -9,7 +9,7 @@ for /f "tokens=*" %%A in (%1) do (
     python osdsn2\analytics\mining.py results out\together\tmp
     python osdsn2\analytics\mining.py matrix out\together\tmp\results out\%%A_minhash.csv
     resources\7za.exe a out\%%A.7z out\source\%%A.log out\together\tmp out\%%A_minhash.csv
-    rm out\%%A_minhash.csv
+    del out\%%A_minhash.csv
     rmdir /Q /S out\objects\%%A.log
 )
 
