1ca26a76ca99e436c89f974ad99672e999cd0297
Wallace <wallacefcardoso@gmail.com>
2019-05-09 06:21:55 -0300

    ANA: get k fix

--- osdsn2-analytics/resources/get_k.bat
+++ osdsn2-analytics/resources/get_k.bat
@@ -12,7 +12,7 @@ for /f "tokens=*" %%A in (%1) do (
         python osdsn2\analytics\kmeans.py kmeans out\matrix\tmp\%%A.csv
         python osdsn2\analytics\kmeans.py konly out\matrix\tmp\%%A.csv 10 out\matrix\tmp\source out\matrix\tmp\destination --better-choice kmeans.log
         resources\7za.exe d out\matrix\%%A.7z out\matrix
-        xcopy kmeans.log out\matrix\tmp\matrix.log
+        copy kmeans.log out\matrix\tmp\matrix.log
         resources\7za.exe a out\matrix\%%A.7z out\matrix\tmp
     )
 )
