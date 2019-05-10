d30e9f31e1a6c1e35b8e9e82707d13e10d691d8c
Wallace <wallacefcardoso@gmail.com>
2019-05-09 21:09:49 -0300

    ANA: check if matrix exists in 7z

--- osdsn2-analytics/resources/get_k.bat
+++ osdsn2-analytics/resources/get_k.bat
@@ -2,18 +2,22 @@
 @echo off
 for /f "tokens=*" %%A in (%1) do (
     if exist out\matrix\%%A.7z (
-        rmdir /Q /S out\matrix\tmp
-        mkdir out\matrix\tmp
-        resources\7za.exe e out\matrix\%%A.7z -oout\matrix\tmp out\%%A.csv
-        resources\7za.exe e out\matrix\%%A.7z -oout\matrix\tmp\source out\together\tmp
-        rmdir /Q /S out\matrix\tmp\source\results
-        mkdir out\matrix\tmp\destination
-        del kmeans.log
-        python osdsn2\analytics\kmeans.py kmeans out\matrix\tmp\%%A.csv
-        python osdsn2\analytics\kmeans.py konly out\matrix\tmp\%%A.csv 10 out\matrix\tmp\source out\matrix\tmp\destination --better-choice kmeans.log
-        resources\7za.exe d out\matrix\%%A.7z out\matrix
-        copy kmeans.log out\matrix\tmp\matrix.log
-        resources\7za.exe a out\matrix\%%A.7z out\matrix\tmp
+        set mainfound=0
+        resource\7za.exe l out\matrix\%%A.7z | findstr /r /c:"out\matrix" >nul && set mainfound=1
+        if %mainfound% == 1 (
+            rmdir /Q /S out\matrix\tmp
+            mkdir out\matrix\tmp
+            resources\7za.exe e out\matrix\%%A.7z -oout\matrix\tmp out\%%A.csv
+            resources\7za.exe e out\matrix\%%A.7z -oout\matrix\tmp\source out\together\tmp
+            rmdir /Q /S out\matrix\tmp\source\results
+            mkdir out\matrix\tmp\destination
+            del kmeans.log
+            python osdsn2\analytics\kmeans.py kmeans out\matrix\tmp\%%A.csv
+            python osdsn2\analytics\kmeans.py konly out\matrix\tmp\%%A.csv 10 out\matrix\tmp\source out\matrix\tmp\destination --better-choice kmeans.log
+            resources\7za.exe d out\matrix\%%A.7z out\matrix
+            copy kmeans.log out\matrix\tmp\matrix.log
+            resources\7za.exe a out\matrix\%%A.7z out\matrix\tmp
+        )
     )
 )
 
