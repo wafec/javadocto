c42dfab8ad5a09f9ab921753ae1e5a6f3bf10e6e
Walace <wallacefcardoso@gmail.com>
2019-05-14 23:26:38 -0300

    ANA: desktop last changes

--- osdsn2-analytics/resources/get_k.bat
+++ osdsn2-analytics/resources/get_k.bat
@@ -7,10 +7,10 @@ for /f "tokens=*" %%A in (%1) do (
         resources\7za.exe e out\matrix\%%A.7z -oout\matrix\tmp out\%%A.csv
         resources\7za.exe x out\matrix\%%A.7z -oout\matrix\tmp\source out\together\tmp\reduce\chosen
         resources\7za.exe x out\matrix\%%A.7z -oout\matrix\tmp\grouped out\together\tmp\reduce\grouped
-        xcopy out\matrix\tmp\source\out\together\tmp\reduce\chosen\* out\matrix\tmp\source
-        rmdir /Q /S out\matrix\tmp\source\out\together\tmp\reduce\chosen
-        xcopy out\matrix\tmp\grouped\out\together\tmp\reduce\grouped\* out\matrix\tmp\grouped
-        rmdir /Q /S out\matrix\tmp\grouped\out\together\tmp\reduce\grouped
+        xcopy /S out\matrix\tmp\source\out\together\tmp\reduce\chosen\* out\matrix\tmp\source
+        xcopy /S out\matrix\tmp\grouped\out\together\tmp\reduce\grouped\* out\matrix\tmp\grouped
+        rmdir /Q /S out\matrix\tmp\grouped\out
+        rmdir /Q /S out\matrix\tmp\source\out
         rmdir /Q /S out\matrix\tmp\source\results
         mkdir out\matrix\tmp\destination
         del kmeans.log
@@ -18,7 +18,7 @@ for /f "tokens=*" %%A in (%1) do (
         python osdsn2\analytics\kmeans.py konly out\matrix\tmp\%%A.csv 10 out\matrix\tmp\source out\matrix\tmp\destination --better-choice kmeans.log
         resources\7za.exe d out\matrix\%%A.7z out\matrix
         copy kmeans.log out\matrix\tmp\matrix.log
-        python osdsn2\analytics\kmeans.py check out\matrix\tmp\destination out\matrix\tmp\grouped
+        REM python osdsn2\analytics\kmeans.py check out\matrix\tmp\destination out\matrix\tmp\grouped
         resources\7za.exe a out\matrix\%%A.7z out\matrix\tmp
     )
 )
