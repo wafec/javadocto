562517861733a61da91581f0af061da45f61401f
Wallace <wallacefcardoso@gmail.com>
2019-06-13 22:31:32 -0300

    New filters

--- osdsn2-analytics/osdsn2/analytics/semantics.py
+++ osdsn2-analytics/osdsn2/analytics/semantics.py
@@ -1,5 +1,57 @@
+import os
+import re
+import json
 
 
-class StateSequences(object):
-    def __init__(self):
-        pass
+class StateParameterFaultRelation(object):
+    def __init__(self, together):
+        self.together = together
+        self.tester = None
+        self.service = None
+        self.start_date = None
+        self.end_date = None
+        self.together_object = None
+        self.map_messages_for_tester = None
+        self.map_traces_per_process = None
+
+    def preprocess(self):
+        for together_file in [os.path.join(self.together, x) for x in os.listdir(self.together)]:
+            if os.path.isfile(together_file):
+                name_m = re.search(r'^tester_(?P<tester>[\w]+)_{2,5}service_(?P<service>[\w]+)_{2,5}(?P<start_date>\w+_\d+_\d+_\d+_\d+_\d+)_{2,5}(?P<end_date>\w\+_\d+_\d+_\d+_\d+_\d+)(\.\w+)?$', os.path.basename(together_file))
+                self.tester = name_m.group('tester')
+                self.service = name_m.group('service')
+                self.start_date = name_m.group('start_date')
+                self.end_date = name_m.group('end_date')
+                with open(together_file, 'r') as together_stream:
+                    self.together_object = json.load(together_stream)
+                self.walk_through_processes()
+
+    def parse_service_name(self, service_name):
+        return re.sub(r'\[\d+\]$', '', service_name)
+
+    def parse_log_time(self, log_line):
+        log_m = re.search(r'^(?P<log_date>\w+\s\d+\s\d+:\d+:\d+)\s', log_line)
+        tester_m = re.search(r'^\w+\s+(?P<log_date>\d+-\d+\d+\s\d+:\d+:\d+(,\d+)?)', log_line)
+        if log_m:
+            return log_m.group('log_date')
+        elif tester_m:
+            return tester_m.group('log_date')
+        return None
+
+    def walk_through_processes(self):
+        self.map_messages_for_tester = []
+        self.map_traces_per_process = {}
+        for entry in self.together_object:
+            for service_name, service_entry in entry['logs'].items():
+                service_name_parsed = self.parse_service_name(service_name)
+                if service_name_parsed not in self.map_traces_per_process:
+                    self.map_traces_per_process[service_name_parsed] = 0
+                self.map_traces_per_process[service_name_parsed] += service_entry[service_name]['traces']
+            for log in entry['tester']:
+                for log_line in log['log_lines']:
+                    log_line_m = re.search(r'Message\smethod=(?P<message_function_name>\S+)$', log_line)
+                    if log_line_m:
+                        time_parsed = self.parse_log_time(log_line)
+                        message_function_name = log_line_m.group('message_function_name')
+                        self.map_messages_for_tester.append((time_parsed, message_function_name))
+
